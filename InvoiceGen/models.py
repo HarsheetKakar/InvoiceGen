cae3c41e1f4574918963b81721ece99d14449638
Harsheet-saxena <harshsaxena1999@gmail.com>
2018-11-03 01:56:51 +0530

    separating all files in different python scripts

---
+++ InvoiceGen/models.py
@@ -0,0 +1,15 @@
+from InvoiceGen import db
+
+class Our_customer(db.Model):
+    __tablename__ = 'Our_customer'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(120))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(30))
+    gstnumber = db.Column(db.Integer())
+    total_receivables = db.Column(db.Integer())
+    total_payable = db.Column(db.Integer())
+    companyname = db.Column(db.String(120))
+    address= db.Column(db.String(120))
+    phone= db.Column(db.Integer())
